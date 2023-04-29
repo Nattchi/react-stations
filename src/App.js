5e1253babb0aba0b122a0d4c5e78a1d1311246b2
Nattchi <48373518+Nattchi@users.noreply.github.com>
2023-04-30 03:51:26 +0900

    Station05

--- src/App.js
+++ src/App.js
@@ -12,12 +12,23 @@ export const App = () => {
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
   )
 
+  async function updateImage() {
+    await fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => {
+        setDogUrl(data.message)
+      })
+  }
+
   return (
     <>
       <header>Dog App</header>
       <div>
         <h2>犬の画像を表示するサイトです</h2>
         <img src={dogUrl} alt="dog" />
+      </div>
+      <div>
+        <button onClick={updateImage}>更新</button>
       </div>
     </>
   )
