6f9f0911a38a524bf871ba64861965f19420a721
Nattchi <48373518+Nattchi@users.noreply.github.com>
2023-04-30 03:40:26 +0900

    Station04

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState(
+    'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg',
+  )
+
   return (
     <>
       <header>Dog App</header>
       <div>
         <h2>犬の画像を表示するサイトです</h2>
-        <img
-          src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"
-          alt="dog"
-        />
+        <img src={dogUrl} alt="dog" />
       </div>
     </>
   )
