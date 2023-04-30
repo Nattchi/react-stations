5cd7b31880434073034ae472b1d42ceb7bc40b27
Nattchi <48373518+Nattchi@users.noreply.github.com>
2023-04-30 15:47:50 +0900

    Station08

--- src/DogImage.js
+++ src/DogImage.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 import React from 'react'
 
-export default function DogImage({ url }) {
-  return <img src={url} alt="dog" />
+export default function DogImage(props) {
+  return <img src={props} alt="dog" />
 }
